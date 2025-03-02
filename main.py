d3fad392e0e0a2d8aca4cd0f8644760623b5ab54
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-03-02 21:06:31 +0530

    Refactor main.py by removing unused message handling routes and cleaning up code structure

--- main.py
+++ main.py
@@ -2,37 +2,16 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import get_db, engine
 import sql_models
-from models import MsgPayload
 
-# Create all tables in the database
 sql_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SchoolSphere API")
-messages_list: dict[int, MsgPayload] = {}
-
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to SchoolSphere API"}
 
-
 # About page route
 @app.get("/about")
 def about() -> dict[str, str]:
     return {"message": "This is the about page."}
-
-
-# Route to add a message
-@app.post("/messages/{msg_name}/")
-def add_msg(msg_name: str) -> dict[str, MsgPayload]:
-    # Generate an ID for the item based on the highest ID in the messages_list
-    msg_id = max(messages_list.keys()) + 1 if messages_list else 0
-    messages_list[msg_id] = MsgPayload(msg_id=msg_id, msg_name=msg_name)
-
-    return {"message": messages_list[msg_id]}
-
-
-# Route to list all messages
-@app.get("/messages")
-def message_items() -> dict[str, dict[int, MsgPayload]]:
-    return {"messages:": messages_list}
